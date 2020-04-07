e58fc3e76821ce165069b141d4bac2c6b4648795
Bartek <bartlomiej@bystrzycki.com>
2020-04-07 13:38:59 +0200

    pokedex added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokecard name="Charmander" id={19} />
+        <Pokedex />
       </div>
     );
   }
